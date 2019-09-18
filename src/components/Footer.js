b801044943c763a8a56299372ab4347032983298
Cameron Kelley <cameron@kelley.vc>
2019-09-17 19:22:09 -0500

    clean footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,37 +14,49 @@ import Logo from './Logo';
 //CSS Components
 const Container = styled.div`
     width: 100%;
-    background-color: #e2e3e5;
+    background-color: #f2f3f5;
     margin-top: 20px;
-    padding: 20px;
-    text-align: center;
+    padding: 20px 30px;
+    // text-align: center;
 
     div {
-        padding: 10px 0 5px 0;
+        // padding: 0 0 5px 0;
         color: black;
+    }
+
+    p.copyright {
+        text-align: center;
+        opacity: 0.6;
+        margin-top: 10px;
+        font-size: 80%;
     }
 `;
 
 const Row = styled.div`
+    ${flexBoxMixinComputer('row', 'space-between', 'left')};
     ${flexBoxMixinMobile('column', 'flex-start', 'center')};
-    ${flexBoxMixinComputer('row', 'space-between', 'center')};
 `;
 
 const Column = styled.div`
-    ${flexBoxMixin('column', 'flex-start', 'center')};
+    ${flexBoxMixin('column', 'flex-start', 'right')};
 `;
 
 const Footer = () => {
     let date = new Date();
     let year = date.getFullYear();
+
+    // <p>
+    //     All The Job Sites pulls together over 200 different job boards<br/> so you can find the right job.
+    // </p>
     return (
         <Container>
             <Row>
-                <Logo />
                 <Column>
+                    <Logo />
+
+                </Column>
+                <Column className="right">
                     <div>
-                        All The Job Sites has over 200 different job boards
-                        helping you to find the right job
                     </div>
                     <div>
                         <Link to="/home" style={{ marginRight: 10 }}>
@@ -56,20 +68,13 @@ const Footer = () => {
                         <Link to="/contact-us" style={{ marginRight: 10 }}>
                             Contact Us
                         </Link>
-                        <Link to="/post-job">Post Job</Link>
+                        <Link to="/post-job">Post a Job</Link>
                     </div>
                 </Column>
             </Row>
-
-            <p
-                style={{
-                    marginBottom: 0,
-                    paddingTop: 10,
-                    borderTop: '1px solid grey',
-                }}
-            >
+            <p className="copyright">
                 Copyright &copy; {year}, All The Job Sites, Inc. "All The Job
-                Sites" and logo are propreity trademarks of All The Job Sites,
+                Sites" and logo are trademarks of All The Job Sites,
                 Inc.
             </p>
         </Container>
